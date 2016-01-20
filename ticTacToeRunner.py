d7a23432cbd92cf6c9fbc2a89e9dbf75ee361cc9
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-20 16:26:48 -0500

    do not edit this file

---
+++ ticTacToeRunner.py
@@ -0,0 +1,9 @@
+# you do not need to edit this file
+# this is the runner for the ticTacToe program
+import ticTacToe
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+ticTacToe.startGame('X', theBoard)

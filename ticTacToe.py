384461d35955cabfaa3735a1eeed64022ef5676f
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-20 16:27:12 -0500

    modify this file

---
+++ ticTacToe.py
@@ -0,0 +1,48 @@
+def printBoard(board):
+    # TO DO #################################################################
+    # Write code in this function that prints the game board.               #
+    # Simply print the code in this function, the user shouldn't interact   #
+    # with the user in any way.                                             #
+    #                                                                       #
+    # Hint: you can follow the same process that was done in the textbook.  #
+    #########################################################################
+
+def checkWinner(board, player):    
+    print('Checking if ' + player + ' is a winner...')
+    
+    # TO DO #################################################################
+    # Write code in this function that checks the tic-tac-toe board          #
+    # to determine if the player stored in variable 'player' currently      #
+    # has a winning position on the board.                                  #
+    # This function should return True if the player specified in           #
+    # variable 'player' has won. The function should return False           #
+    # if the player in the variable 'player' has not won.                   #
+    #########################################################################
+    
+    
+def startGame(startingPlayer, board):
+    # TO DO #################################################################
+    # Add comments to each line in this function to describe what           #
+    # is happening. You do not need to modify any of the Python code        #
+    #########################################################################
+
+    turn = startingPlayer
+    for i in range(9):
+        printBoard(board)
+        print('Turn for ' + turn + '. Move on which space?')
+        move = input()
+        board[move] = turn
+        if( checkWinner(board, 'X') ):
+            print('X wins!')
+            break
+        elif ( checkWinner(board, 'O') ):
+            print('O wins!')
+            break
+    
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+        
+    printBoard(board)
+    
